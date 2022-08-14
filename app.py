c7287fffe47c1cc08759d35b264adf6dab630e09
admin <yuvinarwade@gmail.com>
2022-08-15 02:23:15 +0530

    Final commit with Exception block

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from housing.logger import logger
-
+import sys
+from housing.logger import logging
+from housing.exception import HousingException  
 
 
 app=Flask(__name__)
@@ -8,6 +9,13 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+
+    try:
+        raise Exception("We are testing custome message ")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing_error_message)
+
     logging.info("we are testing logging module")
     return "Hello this is Yuvraj Kishor Narwade"
 
