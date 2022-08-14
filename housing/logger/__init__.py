61bed1d2499d85025e6a477dfd0a493ac5ca6ba1
admin <yuvinarwade@gmail.com>
2022-08-15 01:33:05 +0530

    commit for logger 2

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -9,7 +9,7 @@ CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
 
 
-os.makedirs(LOG_DIR)
+os.makedirs(LOG_DIR,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
